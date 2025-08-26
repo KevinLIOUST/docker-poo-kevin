7e6261e74a44b85d4eef803de7a13c149f2ef637
KevinLIOUST <kevin.lioust@outlook.fr>
2025-08-26 14:19:36 +0200

    Création de la structure  ! :)

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+// header("Location: gallery.php");
+// exit();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercices Fonctions</title>
+
+    <!-- Lien vers Bootstrap -->
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+
+    <!-- Lien vers les icônes Bootstrap -->
+    <link rel="stylesheet" href="../../node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+
+    <!-- Lien vers le fichier pour designer le site web -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
